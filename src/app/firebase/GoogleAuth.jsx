e18983d73b3bbf94963f945de4cf9e7935f5ad33
Gabriel <gabriellikov@gmail.com>
2023-08-13 19:18:29 +0300

    sidebar

--- src/app/firebase/GoogleAuth.jsx
+++ src/app/firebase/GoogleAuth.jsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import { auth } from "./firebase";
 import {
 	GoogleAuthProvider,
@@ -6,31 +7,24 @@ import {
 	onAuthStateChanged,
 } from "firebase/auth";
 
-onAuthStateChanged(auth, (user) => {
-	if (user) {
-		const displayName = user.displayName;
-		const email = user.email;
-		const photoURL = user.photoURL;
-
-		// Display user information
-		document.getElementById("display-name").textContent = displayName;
-		document.getElementById("email").textContent = email;
-		document.getElementById("profile-picture").src = photoURL;
-	} else {
-		// No user is signed in
-		console.log("No user is signed in.");
-	}
-});
-
 export const GoogleAuth = () => {
-	return (
-		<button
-			className="absolute top-3 right-3 border-none bg-white p-2 rounded font-medium text-[#757575]"
-			onClick={async (e) => {
-				const provider = await new GoogleAuthProvider();
-				return signInWithPopup(auth, provider);
-			}}>
-			Sign In with Google
-		</button>
-	);
+	const [userInfo, setUserInfo] = useState(null);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				const displayName = user.displayName;
+				const email = user.email;
+				const photoURL = user.photoURL;
+
+				setUserInfo({ displayName, email, photoURL });
+			} else {
+				setUserInfo(null);
+			}
+		});
+
+		return () => unsubscribe();
+	}, []);
+
+	return userInfo; // Return the userInfo object
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,13 @@
-import { GoogleAuth } from "./firebase/GoogleAuth";
 import { List } from "./firebase/database";
+import { Sidebar } from "./components/Sidebar";
 
 export default function Home() {
 	return (
 		<>
 			<div>
 				<List></List>
-
-				<div className="absolute top-1 left-3 bg-gray-400 py-1 px-3 rounded-3xl flex">
-					<img
-						className="inline rounded-full h-20 w-20 mx-5"
-						id="profile-picture"
-						src=""
-						alt=""
-					/>
-					<div>
-						<h1 className="text-center text-4xl" id="display-name">
-							Sign in!
-						</h1>
-						<h2 className="text-center text-2xl" id="email">
-							Sign in!
-						</h2>
-					</div>
-				</div>
+				<Sidebar></Sidebar>
 			</div>
-
-			<GoogleAuth></GoogleAuth>
 		</>
 	);
 }
