b60f850e4912fdf4ba49f353812c3bb5281597da
Gabriel <gabriellikov@gmail.com>
2023-08-16 03:20:08 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,18 @@ import { Sidebar } from "./components/Sidebar";
 
 export default function Home() {
 	const [selectedListID, setSelectedListID] = useState("default");
-	const [listCount, setListCount] = useState(0);
 
 	return (
 		<>
 			<div className="flex flex-col md:flex-row bg-gradient-to-b from-[#A8DADC] to-[#1D3557]">
 				<div>
 					{/* Pass selectedListID to Sidebar */}
-					<Sidebar
-						setSelectedListID={setSelectedListID}
-						setListCount={setListCount}
-					/>
+					<Sidebar setSelectedListID={setSelectedListID} />
 				</div>
 
 				<div className="flex justify-center md:items-center min-h-screen w-full">
 					{/* Pass selectedListID to List */}
-					<List selectedListID={selectedListID} listCount={listCount} />
+					<List selectedListID={selectedListID} />
 				</div>
 			</div>
 		</>
