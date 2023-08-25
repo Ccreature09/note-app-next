cc450fc94151cf3b3338a8ef247ae417c7891cc9
Gabriel <gabriellikov@gmail.com>
2023-08-26 00:21:10 +0300

    ESlint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'OctoNotes',
-  description: 'Notes for your everyday needs!',
-  manifest: '/manifest.webmanifest'
-}
+   title: 'OctoNotes',
+   description: 'Notes for your everyday needs!',
+   manifest: '/manifest.webmanifest'
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({
+   children
+}: {
+   children: React.ReactNode;
+}) {
+   return (
+      <html lang="en">
+         <body className={inter.className}>{children}</body>
+      </html>
+   );
 }
