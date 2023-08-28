683935dd1269c75650c32fa317c5ecf8912fb9f3
Gabriel <gabriellikov@gmail.com>
2023-08-29 00:26:49 +0300

    simple bug fix

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -304,33 +304,35 @@ export const Sidebar = ({ setSelectedListID }) => {
                                  >
                                     {list.title}
                                     <div className="flex justify-between gap-4 mt-3">
-                                       {list.type == 'group' && (
-                                          <button
-                                             className="w-1/2 text-green-500 bg-[#1D3557] p-1.5 mb-3 rounded"
-                                             onClick={(e) => {
-                                                e.stopPropagation();
-                                                toggleMemberList();
-                                             }}
-                                          >
-                                             <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth={1.5}
-                                                stroke="currentColor"
-                                                className=" h-9 item hover:bg-[#457B9D]  w-full p-2 rounded transition-all duration-200"
+                                       {list.type == 'group' &&
+                                          selectedListID.listID == list.id && (
+                                             <button
+                                                className="w-1/2 text-green-500 bg-[#1D3557] p-1.5 mb-3 rounded"
+                                                onClick={(e) => {
+                                                   e.stopPropagation();
+                                                   toggleMemberList();
+                                                }}
                                              >
-                                                <path
-                                                   strokeLinecap="round"
-                                                   strokeLinejoin="round"
-                                                   d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-                                                />
-                                             </svg>
-                                          </button>
-                                       )}
+                                                <svg
+                                                   xmlns="http://www.w3.org/2000/svg"
+                                                   fill="none"
+                                                   viewBox="0 0 24 24"
+                                                   strokeWidth={1.5}
+                                                   stroke="currentColor"
+                                                   className=" h-9 item hover:bg-[#457B9D]  w-full p-2 rounded transition-all duration-200"
+                                                >
+                                                   <path
+                                                      strokeLinecap="round"
+                                                      strokeLinejoin="round"
+                                                      d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                                                   />
+                                                </svg>
+                                             </button>
+                                          )}
                                        <button
                                           className={` text-red-500 bg-[#1D3557] p-1.5 mb-3 rounded ${
-                                             list.type == 'individual'
+                                             list.type == 'individual' ||
+                                             selectedListID.listID != list.id
                                                 ? 'w-full'
                                                 : 'w-1/2'
                                           }`}
